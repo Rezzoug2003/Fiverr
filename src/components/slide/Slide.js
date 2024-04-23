606039740bfc01d74fed28896cdc6d2bec0e704f
Rezzoug Bilal <bilalrzog@gmail.com>
2024-04-23 09:35:59 +0200

    client side of fiverr

---
+++ src/components/slide/Slide.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./slide.scss";
+import Slider from "infinite-react-carousel";
+
+export const Slide = ({ children, slidesToShow, arrowsScroll })  => {
+  return (
+    <div className="slide">
+      <div className="container">
+        <Slider slidesToShow={slidesToShow} arrowsScroll={arrowsScroll}>
+          {children}
+        </Slider>
+      </div>
+    </div>
+  );
+};
